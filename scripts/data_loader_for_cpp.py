b16300120bf4bdd2ae037423d306d041a5966fb8
allegorywrite <arita.tomoki@icloud.com>
2023-07-09 19:34:03 +0900

    cpp用にデータセット処理を再実装

--- scripts/data_loader_for_cpp.py
+++ scripts/data_loader_for_cpp.py
@@ -2,11 +2,11 @@ import glob
 import numpy as np
 import concurrent.futures
 from multiprocessing import cpu_count
-from torch import nn, tanh, relu
+# from torch import nn, tanh, relu
 import sys
 from pathlib import Path
 import resource
-import torch
+# import torch
 import random 
 import os
 import open3d as o3d
@@ -15,147 +15,202 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import argparse
 import pandas as pd
+import yaml
 
 sys.path.append(str(Path(__file__).parent.parent))
 
 class DataAnalyzer:
-    def __init__(self, visualize=False):
-      if torch.cuda.is_available():
-        self.device = torch.device('cuda')
-      else:
-        self.device = torch.device('cpu')
-      print("Initializing DataAnalyzer on {}...".format(self.device))
-      self.drones_num = 3
-      self.replay_dir = "../data/training/replay/agents{}_*.csv".format(self.drones_num)
-      self.train_dataset = []
-      self.visualize = visualize
-      self.neighbor_data = np.empty((0, 3))
-      self.data_num_max = 100000
+	def __init__(self, visualize=False):
+		# if torch.cuda.is_available():
+		#   self.device = torch.device('cuda')
+		# else:
+		#   self.device = torch.device('cpu')
+		# print("Initializing DataAnalyzer on {}...".format(self.device))
+		self.drones_num = 3
+		self.dim_per_drone = 14
+		self.replay_dir = "../data/training/replay/agents{}_*.csv".format(self.drones_num)
+		self.train_dataset = []
+		self.visualize = visualize
+		self.data_num_max = 100000
+		self.t_start = 0
+		self.goal_horizon = 8
 
-    def generate_dataset(self, replay_file):
-      df = pd.read_csv(replay_file, header=None)
-      state_seq_of_all_drones = df.to_numpy()
-      print("state_seq_of_all_drones.shape = {}".format(state_seq_of_all_drones.shape))
-      # # pcdファイルの読み込み
-      # pcd = o3d.io.read_point_cloud(map_file_path)
-      # # ポイントクラウドデータをnumpy配列に変換
-      # points = np.asarray(pcd.points)
-      # map_file = "{}/../map/{}".format(os.path.dirname(replay_file), os.path.basename(replay_file))
-      # map_data = np.load(map_file, allow_pickle=True)
-      # print("replay_data.shape = {}, map_data.shape = {}".format(replay_data.shape, map_data.shape))
-      # for t in range(0, replay_data.shape[0]-1):
-      #   if replay_data[t,0] == map_data[t,0]:
-      #     print("Time:{} at t:{}".format(map_data[t,0], t))
-      #     point_clouds = map_data[t,1]
-      #     for i in range(0, replay_data.shape[1]-1):
-      #       p_i_world = np.array(replay_data[t,i+1][0:3])
-      #       p_next = np.array(replay_data[t+1,i+1][0:3])
-      #       q_i_world = np.quaternion(replay_data[t,i+1][3], replay_data[t,i+1][4], replay_data[t,i+1][5], replay_data[t,i+1][6])
-      #       v_i_world = np.array(replay_data[t,i+1][7:10])
-      #       w_i_world = np.array(replay_data[t,i+1][10:13])
-      #       observation_world = point_clouds[i]
-      #       # 回転行列の計算
-      #       R_inverse_iw = quaternion.as_rotation_matrix(q_i_world.conjugate())
-      #       # オイラー角の計算
-      #       euler_iw = quaternion.as_euler_angles(q_i_world.conjugate())
-      #       # listデータをopen3dのPointCloudに変換
-      #       pcd = o3d.geometry.PointCloud()
-      #       pcd.points = o3d.utility.Vector3dVector(observation_world)
-      #       # 座標変換
-      #       pcd.translate(-p_i_world)
-      #       pcd.rotate(R_inverse_iw)
-      #       # 座標変換後の点群をlistに変換
-      #       observation_local = np.asarray(pcd.points)
-      #       # アクション
-      #       p_next_local = np.dot(R_inverse_iw, p_next - p_i_world)
+	# get [ State Observation Action ]
+	def getSOA_of_world(self, replay_data, map_data, point_cloud_data, t, agent_id):
+		# Observation(World座標型)
+		observation_world = np.asarray(point_cloud_data.points)
+		# State(World座標型)
+		t_i = replay_data[t,self.dim_per_drone*agent_id]
+		goal_i = map_data["agents"][agent_id]["goal"]
+		p_i_world = np.array(
+			replay_data[t,self.dim_per_drone*agent_id+1:self.dim_per_drone*agent_id+4])
+		# S = [ p, q, v, w ] の場合
+		# q_i_world = np.quaternion(
+		# 	replay_data[t,self.dim_per_drone*agent_id+4], 
+		# 	replay_data[t,self.dim_per_drone*agent_id+5], 
+		# 	replay_data[t,self.dim_per_drone*agent_id+6], 
+		# 	replay_data[t,self.dim_per_drone*agent_id+7])
+		q_i_world = np.quaternion(
+			replay_data[t,self.dim_per_drone*agent_id+7], 
+			replay_data[t,self.dim_per_drone*agent_id+8], 
+			replay_data[t,self.dim_per_drone*agent_id+9], 
+			replay_data[t,self.dim_per_drone*agent_id+10])
+		v_i_world = np.array(
+			replay_data[t,self.dim_per_drone*agent_id+4:self.dim_per_drone*agent_id+7])
+		# S = [ p, q, v, w ] の場合
+		# v_i_world = np.array(
+		# 	replay_data[t,self.dim_per_drone*agent_id+8:self.dim_per_drone*agent_id+11])
+		w_i_world = np.array(
+			replay_data[t,self.dim_per_drone*agent_id+11:self.dim_per_drone*agent_id+14])
+		# Action(World座標型)
+		p_next = np.array(
+			replay_data[t+1,self.dim_per_drone*agent_id+1:self.dim_per_drone*agent_id+4])
 
-      #       for j in range(0, replay_data.shape[1]-1):
-      #         if i != j:
-      #           p_j_world = np.array(replay_data[t,j+1][0:3])
-      #           v_j_world = np.array(replay_data[t,j+1][7:10])
-      #           p_ij_local = np.dot(R_inverse_iw, p_j_world - p_i_world)
-      #           v_ij_local = np.dot(R_inverse_iw, v_j_world - v_i_world) + np.cross(w_i_world, p_ij_local)
-      #           self.neighbor_data = np.concatenate((self.neighbor_data, p_ij_local.reshape(1,-1), v_ij_local.reshape(1,-1)), axis=0)
-                
-      #       if(self.visualize and t == 0 and i == 0):
-      #         print("Visualizing Data at t = {}, agent_id = {}".format(t, i))
-      #         self.visualize_data(replay_data, map_data[t,1], self.neighbor_data, observation_local, p_next_local)
+		return p_i_world, q_i_world, v_i_world, w_i_world, goal_i, observation_world, p_next
+	
+	def transform_to_local(self, replay_data, t, agent_id, p_i_world, q_i_world, v_i_world, w_i_world, goal_i, observation_world, p_next):
+		# Observation(Local座標型)
+		R_inverse_iw = quaternion.as_rotation_matrix(q_i_world.conjugate())
+		euler_iw = quaternion.as_euler_angles(q_i_world.conjugate())
+		pcd = o3d.geometry.PointCloud()
+		pcd.points = o3d.utility.Vector3dVector(observation_world)
+		pcd.translate(-p_i_world)
+		pcd.rotate(R_inverse_iw, center=(0,0,0))
+		observation_local = np.asarray(pcd.points)
+		# Action(Local座標型)
+		p_next_local = np.dot(R_inverse_iw, p_next - p_i_world)
+		# Goal State(Local座標型)
+		goal_i_local = np.dot(R_inverse_iw, goal_i - p_i_world)
+		if np.linalg.norm(goal_i_local) > self.goal_horizon:
+			goal_i_local = goal_i_local / np.linalg.norm(goal_i_local) * self.goal_horizon
+		neighbor_states = np.empty((0, 3))
+		for neighbor_id in range(0, self.drones_num):
+			if neighbor_id != agent_id:
+				# Neighbor State(World座標型)
+				p_j_world = np.array(
+					replay_data[t,self.dim_per_drone*neighbor_id+1:self.dim_per_drone*neighbor_id+4])
+				# S = [ p, q, v, w ] の場合
+				# v_j_world = np.array(
+				# 	replay_data[t,self.dim_per_drone*neighbor_id+8:self.dim_per_drone*neighbor_id+11])
+				v_j_world = np.array(
+					replay_data[t,self.dim_per_drone*neighbor_id+4:self.dim_per_drone*neighbor_id+7])
+				# Neighbor State(Local座標型)
+				p_ij_local = np.dot(R_inverse_iw, p_j_world - p_i_world)
+				v_ij_local = np.dot(R_inverse_iw, v_j_world)
+				# 相対速度で計算する場合
+				# v_ij_local = np.dot(R_inverse_iw, v_j_world - v_i_world) + np.cross(w_i_world, p_ij_local)
+				neighbor_states = np.concatenate((neighbor_states, p_ij_local.reshape(1,-1), v_ij_local.reshape(1,-1)), axis=0)
 
-      #       # データセットの作成(TODO)
-      #       dataset = []
+		return neighbor_states, observation_local, goal_i_local, p_next_local
 
-      #       self.neighbor_data = np.empty((0, 3))
-      #   else:
-      #     print("time sync error")
-      dataset = []
-      return dataset
+	def generate_dataset(self, replay_file):
+		# リプレイデータの読み込み
+		df = pd.read_csv(replay_file, header=None)
+		replay_data = df.to_numpy()
+		# マップデータの読み込み
+		basename_without_ext, ext = os.path.splitext(os.path.basename(replay_file))
+		map_file = "{}/../map/{}.yaml".format(os.path.dirname(replay_file), basename_without_ext)
+		map_data = yaml.load(open(map_file), Loader=yaml.FullLoader)
+		print("state_seq_of_all_drones.shape = {}".format(replay_data.shape))
+		for t in range(0, replay_data.shape[0]-1):
+			observation_world_array = []
+			if replay_data[t,0] == 0:
+				self.t_start = t + 1
+				continue
+			for agent_id in range(0, self.drones_num):
+				# 点群の読み込み
+				vision_file_path = "{}/../vision/{}.pcd_agent{}_timestep{}.pcd".format(os.path.dirname(replay_file), basename_without_ext, agent_id, t)
+				point_cloud_data = o3d.io.read_point_cloud(vision_file_path)
+				# State, Observation, Actionの取得
+				p_i_world, q_i_world, v_i_world, w_i_world, goal_i, observation_world, p_next = self.getSOA_of_world(replay_data, map_data, point_cloud_data, t, agent_id)
+				observation_world_array.append(observation_world)
+				# ローカル座標系に変換
+				neighbor_state_local_array, observation_local, goal_local, p_next_local = self.transform_to_local(replay_data, t, agent_id, p_i_world, q_i_world, v_i_world, w_i_world, goal_i, observation_world, p_next)
 
-    def load_data(self):
-      print("Loading Data...")
-      files = glob.glob(self.replay_dir)
-      print("Size of files: ", len(files))
-      len_case = 0
-      if self.visualize:
-        self.generate_dataset(files[0])
-        self.visualize = False
-      with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count()) as executor:
-        itr = 0
-        for dataset in executor.map(self.generate_dataset, files):
-          len_case += len(dataset)
-          itr += 1
-          print('files = {}, len_case = {}'.format(itr, len_case))
-          if len_case > self.data_num_max:
-            break
-          self.train_dataset.extend(dataset)
-      print('Total Training Dataset Size: ',len(self.train_dataset))
+			if(self.visualize and t > 130):
+				self.visualize = False
+				print("Visualizing Data at t = {}, agent_id = {}".format(t, agent_id))
+				self.visualize_data(replay_data, observation_world_array, neighbor_state_local_array, observation_local, goal_local, p_next_local, t, q_i_world)
+		# データセットの作成(TODO)
+		dataset = []
+		return dataset
 
-    def visualize_data(self, replay_data, point_clouds_world, replay_data_local, point_cloud_local, p_next_local):
-      # ワールド座標系
-      fig = plt.figure()
-      ax = fig.add_subplot(111, projection="3d")
-      ax.set_xlabel("x")
-      ax.set_ylabel("y")
-      ax.set_zlabel("z")
-      ax.set_xlim(-15, 15)
-      ax.set_ylim(-15, 15)
-      ax.set_zlim(0, 15)
-      colors = ["r", "g", "b", "c", "m", "y", "k"]
-      for agent_id in range(0, replay_data.shape[1]-1):
-        # エージェントの軌跡の描画
-        trajectory_x = []
-        trajectory_y = []
-        trajectory_z = []
-        for t in range(0, replay_data.shape[0]):
-            trajectory_x.append(replay_data[t,agent_id+1][0])
-            trajectory_y.append(replay_data[t,agent_id+1][1])
-            trajectory_z.append(replay_data[t,agent_id+1][2])
-        ax.plot(trajectory_x, trajectory_y, trajectory_z, 
-                color=colors[agent_id % len(colors)], label="drone{}".format(agent_id))
-        # 点群の描画
-        point_cloud = np.array(point_clouds_world[agent_id])
-        ax.scatter(point_cloud[:,0], point_cloud[:,1], point_cloud[:,2], 
-                    color=colors[agent_id % len(colors)], s=1)
-      plt.legend()
+	def load_data(self):
+		print("Loading Data...")
+		files = glob.glob(self.replay_dir)
+		print("Size of files: ", len(files))
+		len_case = 0
+		with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count()) as executor:
+			itr = 0
+			for dataset in executor.map(self.generate_dataset, files):
+				len_case += len(dataset)
+				itr += 1
+				print('files = {}, len_case = {}'.format(itr, len_case))
+				if len_case > self.data_num_max:
+					break
+				self.train_dataset.extend(dataset)
+		print('Total Training Dataset Size: ',len(self.train_dataset))
 
-      # ローカル座標系(t=0)
-      fig2 = plt.figure()
-      ax2 = fig2.add_subplot(111, projection="3d")
-      ax2.set_xlabel("x")
-      ax2.set_ylabel("y")
-      ax2.set_zlabel("z")
-      for agent_id in range(0, int(replay_data_local.shape[0]/2)):
-        neighbor_pos = replay_data_local[agent_id*2] # x, y, z
-        neighbor_vel = replay_data_local[agent_id*2+1] # vx, vy, vz
-        ax2.scatter(neighbor_pos[0], neighbor_pos[1], neighbor_pos[2], 
-                    color=colors[agent_id % len(colors)], s=10)
-        ax2.quiver(neighbor_pos[0], neighbor_pos[1], neighbor_pos[2], 
-                    neighbor_vel[0], neighbor_vel[1], neighbor_vel[2], 
-                    color=colors[agent_id % len(colors)], length=0.5, normalize=True)
-      ax2.scatter(point_cloud_local[:,0], point_cloud_local[:,1], point_cloud_local[:,2], 
-                    color=colors[agent_id % len(colors)], s=1)
-      ax2.quiver(0, 0, 0, p_next_local[0], p_next_local[1], p_next_local[2], 
-                    color="black", length=0.5, normalize=True)
-      plt.show()
+	def visualize_data(self, replay_data, observation_world_array, neighbor_state_local_array, observation_local, goal_local, p_next_local, t, q_i_world):
+		# ワールド座標系
+		fig = plt.figure()
+		ax = fig.add_subplot(111, projection="3d")
+		ax.set_xlabel("x")
+		ax.set_ylabel("y")
+		ax.set_zlabel("z")
+		ax.set_xlim(-15, 15)
+		ax.set_ylim(-15, 15)
+		ax.set_zlim(0, 15)
+		ax.set_title("Episode on global map")
+		colors = ["r", "g", "b", "c", "m", "y", "k"]
+		for agent_id in range(0, self.drones_num):
+			# エージェントの軌跡の描画
+			trajectory_x = replay_data[self.t_start:,self.dim_per_drone*agent_id+1]
+			snapshot_x = replay_data[t,self.dim_per_drone*agent_id+1]
+			snapshot_vx = replay_data[t,self.dim_per_drone*agent_id+4]
+			trajectory_y = replay_data[self.t_start:,self.dim_per_drone*agent_id+2]
+			snapshot_y = replay_data[t,self.dim_per_drone*agent_id+2]
+			snapshot_vy = replay_data[t,self.dim_per_drone*agent_id+5]
+			trajectory_z = replay_data[self.t_start:,self.dim_per_drone*agent_id+3]
+			snapshot_z = replay_data[t,self.dim_per_drone*agent_id+3]
+			snapshot_vz = replay_data[t,self.dim_per_drone*agent_id+6]
+			ax.plot(trajectory_x, trajectory_y, trajectory_z, 
+							color=colors[agent_id % len(colors)], label="drone{}".format(agent_id))
+			ax.scatter(snapshot_x, snapshot_y, snapshot_z, color="black", s=10)
+			ax.quiver(snapshot_x, snapshot_y, snapshot_z, snapshot_vx, snapshot_vy, snapshot_vz, color="black", length=1.0, normalize=True)
+			# 点群の描画
+			point_cloud = np.array(observation_world_array[agent_id])
+			ax.scatter(point_cloud[:,0], point_cloud[:,1], point_cloud[:,2], color=colors[agent_id % len(colors)], s=1)
+		plt.legend()
+    # ローカル座標系(t=0)
+		fig2 = plt.figure()
+		ax2 = fig2.add_subplot(111, projection="3d")
+		ax2.set_xlabel("x")
+		ax2.set_ylabel("y")
+		ax2.set_zlabel("z")
+		ax2.scatter(0, 0, 0, color="black", s=10)
+		ax2.set_title("Snapshot on local map (Dataset) at t = {}".format(t))
+		for agent_id in range(0, int(neighbor_state_local_array.shape[0]/2)):
+			neighbor_pos = neighbor_state_local_array[agent_id*2] # x, y, z
+			neighbor_vel = neighbor_state_local_array[agent_id*2+1] # vx, vy, vz
+			ax2.scatter(neighbor_pos[0], neighbor_pos[1], neighbor_pos[2], 
+									color=colors[agent_id % len(colors)], s=10)
+			ax2.quiver(neighbor_pos[0], neighbor_pos[1], neighbor_pos[2], 
+									neighbor_vel[0], neighbor_vel[1], neighbor_vel[2], 
+									color=colors[agent_id % len(colors)], length=1.0, normalize=True)
+		ax2.scatter(observation_local[:,0], observation_local[:,1], observation_local[:,2], 
+									color=colors[agent_id % len(colors)], s=1)
+		ax2.scatter(goal_local[0], goal_local[1], goal_local[2], color="black", s=20)
+		# Orientationの描画
+		# orientation = np.array([1, 0, 0])
+		# R_iw = quaternion.as_rotation_matrix(q_i_world)
+		# orientation = np.dot(R_iw, orientation)
+		# ax2.quiver(0, 0, 0, orientation[0], orientation[1], orientation[2], 
+		# 							color="red", length=1.0, normalize=True)
+		# Actionの描画
+		ax2.quiver(0, 0, 0, p_next_local[0], p_next_local[1], p_next_local[2], 
+									color="black", length=1.0, normalize=True)
+		plt.show()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
