e423075b01bdea51e24d86a2a02e2c8e4d151801
arita tomoki <tomoking@s125.lab>
2023-09-30 18:42:56 +0900

    first commit

---
+++ ppo_wdail/scripts/train.py
@@ -0,0 +1,53 @@
+import torch
+import os
+import yaml
+import shared_constants
+
+from swarm.models.polkadot import Polkadot
+from gym_pybullet_drones.envs.multi_agent_rl.PolkadotAviary import PolkadotAviary
+from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import ActionType, ObservationType
+
+# Wasserstein Distance guided Adversarial Imitation Learning (WDAIL)の学習
+
+def train():
+    print("hello world")
+
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    # パラメータの読み込み
+    yaml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../config/params.yaml")
+    with open(yaml_path, 'r') as f:
+        params = yaml.load(f, Loader=yaml.SafeLoader)
+
+    # 環境の登録
+    obs_type = ObservationType.VIS
+    act_type = ActionType.VEL5D
+    env = PolkadotAviary(num_drones=params["env"]["num_drones"],
+                           aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+                           obs=obs_type,
+                           act=act_type,
+                           freq=params["env"]["frequency"],
+                           goal_position=(1, 1, 1),
+                           gui=False,
+                           record=False
+                           )
+    
+    # エージェント(生成モデル)の登録
+    obs_space = env.observation_space[0]
+    action_space = env.action_space[0]
+    print("obs_space: ", obs_space)
+    print("action_space: ", action_space)
+    num_outputs = action_space.shape[0]
+    model_config = ppo.DEFAULT_CONFIG.copy()
+    agent = Polkadot(obs_space, action_space, num_outputs, model_config, name)
+
+    # 識別モデルの登録
+    # データバッファの登録
+    # データセットの登録
+    # モデルの学習
+
+if __name__ == "__main__":
+    train()
